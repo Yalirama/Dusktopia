a4d5c41e4e022494ef637cdb0d1b015ea5abed04
yalirama <ybaurzhanuly@inbox.ru>
2023-09-26 21:21:36 +0600

    adding factions more blocks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,3 +20,45 @@ factionsItems.forEach(item => {
 				item.classList.add('active')
 		})
 })
+
+const factionsMoreInner = document.querySelectorAll('.factionsMoreInner');
+
+factionsMoreInner.forEach((item, i) => {
+  switch (i) {
+    case 0:
+      item.style.background = "url('/assets/img/bg/badlands.png') center / cover no-repeat";
+      break;
+    case 1:
+      item.style.background = "url('/assets/img/bg/glacial-frontier.png') center / cover no-repeat";
+      break;
+    case 2:
+      item.style.background = "url('/assets/img/bg/sundered-grove.png') center / cover no-repeat";
+      break;
+    case 3:
+      item.style.background = "url('/assets/img/bg/sky-citadel.png') center / cover no-repeat";
+      break;
+    default:
+      break;
+  }
+});
+const factionsItemBody = document.querySelectorAll('.factionsItemBody .button');
+const factionsMoreClose = document.querySelectorAll('.button.close')
+
+factionsMoreClose.forEach(item => {
+	item.addEventListener('click', (e) => {
+		e.preventDefault();
+		factionsMoreInner.forEach(inner => {
+      inner.classList.remove('active');
+    });
+	})
+})
+
+factionsItemBody.forEach((item, i) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    factionsMoreInner.forEach(inner => {
+      inner.classList.remove('active');
+    });
+    factionsMoreInner[i].classList.toggle('active');
+  });
+});

f29852bcb51eaed6d345169ac26c7336b573e6d2
yalirama <ybaurzhanuly@inbox.ru>
2023-09-26 14:24:47 +0600

    adding intro mobile and burger with menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const burger = document.querySelector('.burger'),
+			intro = document.querySelector('.intro'),
+			check = document.querySelector('#check')
+
+burger.addEventListener('click', () => {
+	if (check.checked) {
+		intro.classList.add('active')
+	} else {
+		intro.classList.remove('active')
+	}
+})

e656dd2a0ce95cdb1c04f61740828dbb9b1001c8
yalirama <ybaurzhanuly@inbox.ru>
2023-09-27 11:32:04 +0600

    adding factions functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,44 +21,57 @@ factionsItems.forEach(item => {
 		})
 })
 
-const factionsMoreInner = document.querySelectorAll('.factionsMoreInner');
+const factionsMoreInner = document.querySelectorAll('.factionsMoreInner')
 
 factionsMoreInner.forEach((item, i) => {
   switch (i) {
     case 0:
-      item.style.background = "url('/assets/img/bg/badlands.png') center / cover no-repeat";
-      break;
+      item.style.background = "url('/assets/img/bg/badlands.png') center / cover no-repeat"
+      break
     case 1:
-      item.style.background = "url('/assets/img/bg/glacial-frontier.png') center / cover no-repeat";
-      break;
+      item.style.background = "url('/assets/img/bg/glacial-frontier.png') center / cover no-repeat"
+      break
     case 2:
-      item.style.background = "url('/assets/img/bg/sundered-grove.png') center / cover no-repeat";
-      break;
+      item.style.background = "url('/assets/img/bg/sundered-grove.png') center / cover no-repeat"
+      break
     case 3:
-      item.style.background = "url('/assets/img/bg/sky-citadel.png') center / cover no-repeat";
-      break;
+      item.style.background = "url('/assets/img/bg/sky-citadel.png') center / cover no-repeat"
+      break
     default:
-      break;
+      break
   }
-});
-const factionsItemBody = document.querySelectorAll('.factionsItemBody .button');
-const factionsMoreClose = document.querySelectorAll('.button.close')
+})
+
+function factionsMoreCloseFunc() {
+  factionsMoreInner.forEach(inner => {
+    inner.classList.remove('active')
+  })
+}
+
+const factionsItemBody = document.querySelectorAll('.factionsItemBody .button')
+const factionsMoreClose = document.querySelectorAll('.factionsMoreInnerHead .button.close')
+const factionsMoreNext = document.querySelectorAll('.factionsMoreInnerHead .button.next')
 
 factionsMoreClose.forEach(item => {
-	item.addEventListener('click', (e) => {
-		e.preventDefault();
-		factionsMoreInner.forEach(inner => {
-      inner.classList.remove('active');
-    });
-	})
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+    factionsMoreCloseFunc()
+  })
 })
 
 factionsItemBody.forEach((item, i) => {
   item.addEventListener('click', (e) => {
-    e.preventDefault();
-    factionsMoreInner.forEach(inner => {
-      inner.classList.remove('active');
-    });
-    factionsMoreInner[i].classList.toggle('active');
-  });
-});
+    e.preventDefault()
+    factionsMoreCloseFunc()
+    factionsMoreInner[i].classList.toggle('active')
+  })
+})
+
+factionsMoreNext.forEach((item, i) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+    const nextIndex = (i + 1) % factionsMoreInner.length
+    factionsMoreCloseFunc()
+    factionsMoreInner[nextIndex].classList.add('active')
+  })
+})
